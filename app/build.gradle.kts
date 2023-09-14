748cae162266df730017634fabda72273a75c842
Hongzhe Du <hongzhedu@HongzhedeMBP.askey.com>
2023-09-14 02:42:44 -0700

    XunFei jarfile src

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -32,18 +32,13 @@ android {
     kotlinOptions {
         jvmTarget = "1.8"
     }
-    sourceSets {
-        getByName("main") {
-            jniLibs.srcDirs("src/main/jniLibs")
-        }
-    }
 }
 
 dependencies {
     implementation("androidx.core:core-ktx:1.9.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.9.0")
-    implementation(files("libs/Msc.jar"))
+    implementation(files("src/libs/Msc.jar"))
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
