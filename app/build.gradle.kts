bcd5605822be085ee9752839c083770e73481f40
Hongzhe Du <hongzhedu@HongzhedeMBP.askey.com>
2023-09-14 02:26:29 -0700

    XunFei jarfile

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -32,17 +32,23 @@ android {
     kotlinOptions {
         jvmTarget = "1.8"
     }
+    sourceSets {
+        getByName("main") {
+            jniLibs.srcDirs("src/main/jniLibs")
+        }
+    }
 }
 
 dependencies {
     implementation("androidx.core:core-ktx:1.9.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.9.0")
+    implementation(files("libs/Msc.jar"))
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
     implementation("com.airbnb.android:lottie:6.1.0")
     implementation("com.squareup.okhttp3:okhttp:4.9.0")
-    implementation(files("jniLibs/Msc.jar"))
     implementation("com.google.code.gson:gson:2.8.9")
+
 }
